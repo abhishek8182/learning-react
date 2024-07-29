16f75035e21c2ba70af2ad8e2dc82d9a9405f55f
abhishek8182 <abhishektripathi3196@gmail.com>
2024-07-29 09:24:12 +0530

    first react and vite-react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai or code || Abhishek Tripathi</h1>
+  );
+}
+
+export default App;
---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Chai or react || Abhishek Tripathi</h1>
+  )
+}
+
+export default App
